75bcbf129d38fd2dd0ce2dff848db84c17029168
wilsonkisanya <wilsonkisanya@gmail.com>
2021-03-08 05:11:03 +0300

    responsive fix

--- platform/themes/insulink/partials/header.blade.php
+++ platform/themes/insulink/partials/header.blade.php
@@ -7,6 +7,8 @@
     <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1"
         name="viewport" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Insulink Kenya Ltd, the top insurer in East Africa. Your trusted insurance partner">
+    <meta name="keywords" content="Best Insurer,Best Agency, Best Insurance Agency,Insurance Agency,Top Insurer,Buy,Insure,Claim,Insurance,Health Insurance,Medical Cover,Medical Insurance,Motor,Home,Travel,Pension,Retirement,Travel Cover,Benefits,Beneficiaries,Annuity,Trusted Insurer,investment,investments,life cover,life insurance,motor vehicle,investing,investor">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-migrate-3.3.2.min.js"
